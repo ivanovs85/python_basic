314f3fbf65f9fca296f7acf8d42c0625cb3476c4
Semen <ivanovs85@mail.ru>
2021-02-24 18:34:39 +0300

    создал файл

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+num = (input('Введите число: '))
+var = 0
+num_summ = 0
+for i in num:
+    num_summ += int(i)
+    var += 1
+diff = num_summ - var
+print('Сумма цифр:', num_summ, '\nКол-во цифр в числе:', var, '\nРазность суммы и кол-ва цифр:', diff)
