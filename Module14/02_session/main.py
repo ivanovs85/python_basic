b99de9d3caaf923b2260d578a5ca6339456948fc
Semen <ivanovs85@mail.ru>
2021-02-24 18:25:00 +0300

    Добавил проверку ввода, чтоб Х первой точки не совпадал с Х второй точки

--- Module14/02_session/main.py
+++ Module14/02_session/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+while True:
+    print("Введите первую точку")
+    x1 = float(input('X: '))
+    y1 = float(input('Y: '))
+    print("\nВведите вторую точку")
+    x2 = float(input('X: '))
+    y2 = float(input('Y: '))
+    if x1 == x2:
+        print('Х первой точки не должен быть равен Х второй точки. Введите координаты еще раз.')
+    else:
+        break
+
+x_diff = x1 - x2
+y_diff = y1 - y2
+k = y_diff / x_diff
+b = y2 - k * x2
+
+print("Уравнение прямой, проходящей через эти точки:")
+print("y = ", k, " * x + ", b)
+
