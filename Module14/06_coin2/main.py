19446f1c768ca4499ca12bc10429c39ba54721f6
Semen <ivanovs85@mail.ru>
2021-02-25 12:16:19 +0300

    создал файл

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+print('Введите координаты монетки:')
+x = float(input('Х: '))
+y = float(input('Y: '))
+radius = float(input('Введите радиус: '))
+if x ** 2 + y ** 2 < radius ** 2:
+    print('Монетка где-то рядом')
+else:
+    print('Монетки рядом нет')
