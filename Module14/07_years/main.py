c1fab62f6b1a8de439104ee19c8e2ca35490f225
Semen <ivanovs85@mail.ru>
2021-02-25 14:42:06 +0300

    создал файл

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+first_year = int(input('Введите первый год: '))
+second_year = int(input('Введите первый год: '))
+count = 0
+
+for year in range(first_year, second_year + 1):
+    for num in str(year):
+        for check_num in str(year):
+            if num == check_num:
+                count += 1
+        if count == 3:
+            print(year)
+            count =0
+            break
+        else:
+            count = 0
+
