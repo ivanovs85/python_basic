7b19065785effe6dca696addbd352d42d46da1ba
Semen <ivanovs85@mail.ru>
2021-02-25 11:59:24 +0300

    создал файл

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,3 +1,20 @@
-# TODO здесь писать код
+first_num = input('Введите первое число: ')
+second_num = input('Введите второе число: ')
 
+def revers(num):
+    revers_num = ''
+    revers_int = ''
+    for i in num:
+        if i == '.':
+            revers_int = revers_num + '.'
+            revers_num = ''
+            continue
+        revers_num = i + revers_num
+    revers_num = revers_int + revers_num
+    return revers_num
 
+first_revers = revers(first_num)
+second_revers = revers(second_num)
+summ = float(first_revers) + float(second_revers)
+
+print('Первое число наоборот:', first_revers, '\nВторое число наоборот:', second_revers, '\nСумма:', summ)
