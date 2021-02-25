f246b050160be1e6d414b22020b3c3ebf717d32a
Semen <ivanovs85@mail.ru>
2021-02-25 12:06:27 +0300

    создал файл

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,5 @@
-# TODO здесь писать код
+num = int(input('Введите число: '))
+for devider in range(2, num + 1):
+    if num % devider == 0:
+        break
+print('Наименьший делитель, отличный от еденицы:', devider)
