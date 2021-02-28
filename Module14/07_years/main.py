5a8b9a65425c686e3e567a984b9cead0ab6144c7
Semen <ivanovs85@mail.ru>
2021-02-28 17:22:09 +0300

    создал файл

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -13,17 +13,16 @@ for year in range(first_year, second_year + 1):
             num_first = num
         elif count == 2:
             num_second = num
-
         if num_first == num:
             count_first += 1
         elif num_second == num:
             count_second += 1
 
-if count_first == 3 or count_second == 3:
-    print(year)
+    if count_first == 3 or count_second == 3:
+        print(year)
 
-count = 0
-count_first = 0
-count_second = 0
-num_first = 0
-num_second = 0
+    count = 0
+    count_first = 0
+    count_second = 0
+    num_first = 0
+    num_second = 0
