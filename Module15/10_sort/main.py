b1fb9779b58a645c5ff9d8c582649129025a57eb
Semen <ivanovs85@mail.ru>
2021-03-11 17:29:57 +0300

    New file

--- Module15/10_sort/main.py
+++ Module15/10_sort/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+print('Введите список цифр:')
+list_num = []
+
+for _ in range(5):
+    num = int(input('->'))
+    list_num.append(num)
+
+# Проходимся по списку и меняем местами соседние числа по возрастанию. И это повторяем 5 раз пока точно все не
+# упорядочится
+for _ in range(5):
+    for i in range(4):
+        if list_num[i] > list_num[i + 1]:
+            temp = list_num[i]
+            list_num[i] = list_num[i + 1]
+            list_num[i + 1] = temp
+
+print(list_num)
