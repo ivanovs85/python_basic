d9573121e7ccb316736467e6c157e6c6a69bb7c6
Semen <ivanovs85@mail.ru>
2021-03-11 17:02:23 +0300

    New file

--- Module15/09_word_analysis_2/main.py
+++ Module15/09_word_analysis_2/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+word = input('Введите слово: ')
+letter_list = []
+count = 0
+count_double = 0
+
+for letter in word:
+    letter_list.append(letter)
+
+for letter in letter_list:
+    count += 1
+    if count > len(letter_list) / 2 + 0.5:
+        break
+    elif letter == letter_list[-count]:
+        count_double += 1
+
+if count_double + 1 >= len(letter_list) / 2 + 0.5:
+    print('Слово является полиндромом')
+else:
+    print('Слово не является полиндромом')
