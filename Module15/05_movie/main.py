075a9141d51830ed865561d251caa532e17079d0
Semen <ivanovs85@mail.ru>
2021-03-11 16:50:11 +0300

    New file

--- Module15/05_movie/main.py
+++ Module15/05_movie/main.py
@@ -1,5 +1,22 @@
 films = ['Крепкий орешек', 'Назад в будущее', 'Таксист',
          'Леон', 'Богемская рапсодия', 'Город грехов',
          'Мементо', 'Отступники', 'Деревня']
+check = True
+films_favorite = []
+add = int(input('Сколько фильмов хотите добавить: '))
 
-# TODO здесь писать код
+for _ in range(add):
+    add_film = input('Введите название фильма: ')
+    for film in films:
+        if film == add_film:
+            films_favorite.append(film)
+            check = False
+            break
+    if check:
+        print('Такого фильма нет в списке')
+    check = True
+
+print('\nСписок любимых фильмов:', end=' ')
+
+for i in films_favorite:
+    print(i, end=' ')
