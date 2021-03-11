c72ab9377389b3ac2c9a4eaa3aa28d9fdd2e0eca
Semen <ivanovs85@mail.ru>
2021-03-11 16:36:38 +0300

    New file

--- Module15/02_tournament/main.py
+++ Module15/02_tournament/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+list_participants = ['Артемий', 'Борис', 'Влад', 'Гоша', 'Дима', 'Евгений', 'Женя', 'Захар']
+new_list = []
+
+for index in range(len(list_participants)):
+    if index % 2 != 0:
+       new_list.append(list_participants[index])
+
+print(new_list)
