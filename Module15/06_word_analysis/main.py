e71ff1ca2695a89a83ca126b771e2684e5d9f855
Semen <ivanovs85@mail.ru>
2021-03-11 17:45:19 +0300

    New file

--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -1,2 +1,14 @@
-# TODO здесь писать код
-# Не осилил
+word = input('Введите слово: ')
+word_list = list(word)
+count = 0
+
+for i in range(len(word_list)):
+    for j in range(i + 1, len(word_list)):
+        if word_list[i] == word_list[j]:
+            word_list[i] = 0
+
+for sym in word_list:
+    if sym != 0:
+        count += 1
+
+print('Количество уникальных букв:', count)
