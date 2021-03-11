e854762565bce52ddd3e5faa57566162f826eda2
Semen <ivanovs85@mail.ru>
2021-03-11 16:41:10 +0300

    New file

--- Module15/03_cells/main.py
+++ Module15/03_cells/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+cell = int(input('Введите количество клеток: '))
+unsuitable_list = []
+
+for rank in range(cell):
+  print('Эффективность', rank + 1, 'клетки: ', end = '')
+  efficiency = int(input())
+  if efficiency < rank + 1:
+    unsuitable_list.append(efficiency)
+
+print('Неподходящие значения: ', end = '')
+
+for i in unsuitable_list:
+  print(i, end = ' ')
