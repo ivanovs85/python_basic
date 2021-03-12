ed8bf0b63dc532b8d92604ac9ef45aa23380d10e
Semen <ivanovs85@mail.ru>
2021-03-12 16:30:04 +0300

    New file

--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -3,22 +3,15 @@ card_list = []
 new_card_list = []
 max_card = 0
 
-# TODO, как реализовать range таким образом, чтобы не производить в цикле вычисления (+1) с переменной цикла?
-#  В таком случае, вычисление произойдёт только 1 раз в range, вместо вычислений каждую итерацию цикла.
-
-for index in range(quantity):
-    print(index + 1, 'видеокарта: ', end='')
+for index in range(1, quantity + 1):
+    print(index, 'видеокарта: ', end='')
     card = int(input())
     card_list.append(card)
     if card > max_card:
         max_card = card
 
-
-# TODO, Если переменную цикла используем в коде, её необходимо назвать так, чтобы название отражало суть содержания.
-#  "i" не отражает =)
-
-for i in card_list:
-    if i != max_card:
-        new_card_list.append(i)
+for card in card_list:
+    if card != max_card:
+        new_card_list.append(card)
 
 print('Старый список видеокарт:', card_list, '\nНовый список видеокарт:', new_card_list)
