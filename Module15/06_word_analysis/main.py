0ac00f58cfbf85548a05939d8361c43ef45f5b47
Semen <ivanovs85@mail.ru>
2021-03-12 16:21:00 +0300

    New file

--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -1,23 +1,19 @@
 word = input('Введите слово: ')
 word_list = list(word)
 count = 0
+sym_last = ''
+
+for index in range(len(word_list)):
+    for index_repeat in range(index + 1, len(word_list)):
+        if word_list[index] == word_list[index_repeat] or sym_last == word_list[index_repeat] and sym_last != 0:
+            sym_last = word_list[index]
+            word_list[index] = 0
+            word_list[index_repeat] = 0
 
 
-# TODO, Если переменную цикла используем в коде, её необходимо назвать так, чтобы название отражало суть содержания.
-#  "i" и "j" не отражают =)
-
-for i in range(len(word_list)):
-    for j in range(i + 1, len(word_list)):
-        if word_list[i] == word_list[j]:
-            word_list[i] = 0
-
+print(word_list)
 for sym in word_list:
     if sym != 0:
         count += 1
 
 print('Количество уникальных букв:', count)
-
-
-# TODO Введите слово: карамба
-#  Количество уникальных букв: 5
-#  По идее, буквы 4 =) к р м б
