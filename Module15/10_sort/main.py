f030ce7e149344b2022d5ef524b9b96cee61a545
Semen <ivanovs85@mail.ru>
2021-03-12 16:42:11 +0300

    New file

--- Module15/10_sort/main.py
+++ Module15/10_sort/main.py
@@ -8,11 +8,11 @@ for _ in range(5):
 # Проходимся по списку и меняем местами соседние числа по возрастанию. И это повторяем 5 раз пока точно все не
 # упорядочится
 for _ in range(5):
-    for i in range(4):
-        if list_num[i] > list_num[i + 1]:
-            temp = list_num[i]
-            list_num[i] = list_num[i + 1]
-            list_num[i + 1] = temp
+    for index in range(4):
+        if list_num[index] > list_num[index + 1]:
+            temp = list_num[index]
+            list_num[index] = list_num[index + 1]
+            list_num[index + 1] = temp
 
 print(list_num)
 
