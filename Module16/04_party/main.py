28ff3da46f0bef84a52c9d4ff623b80a9c24b590
Semen <ivanovs85@mail.ru>
2021-03-15 20:14:41 +0300

    Add lesson

--- Module16/04_party/main.py
+++ Module16/04_party/main.py
@@ -1,3 +1,31 @@
+def is_guest_exist(person, guests_list):
+    for name_person in guests_list:
+        if name_person == person:
+            return True
+    return False
+
+
 guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
 
-# TODO здесь писать код
+while True:
+    print('\nСейчас на вечеринке', len(guests), 'человек', guests)
+    command = input('Гость пришел или ушел? ')
+    if command == 'пришел' and len(guests) < 6:
+        name = input('Имя гостя: ')
+        guests.append(name)
+        print('Привет,', name + '!')
+    elif command == 'пришел' and len(guests) >= 6:
+        name = input('Имя гостя: ')
+        print('Прости,', name, ', но мест нет.')
+    elif command == 'ушел':
+        name = input('Имя гостя: ')
+        if is_guest_exist(name, guests):
+            print('Пока,', name)
+            guests.remove(name)
+        else:
+            print('Гостя с таким именем не было')
+    elif command == 'Пора спать':
+        break
+    else:
+        print('Такой команды нету')
+print('Вечеринка закончилась, все легли спать')
--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+players = int(input('Кол-во человек: '))
+num = int(input('Какое число в считалке? '))
+print('Значит, выбывает каждый', num, 'человек')
+
+players_list = []
+start_count = 1
+i_gone = 0
+
+for player in range(1, players + 1):
+    players_list.append(player)
+
+while len(players_list) > 1:
+    print('\nТекущий круг людей:', players_list,
+          '\nНачало счета с номера', start_count)
+    i_gone = i_gone + num % len(players_list) - 1
+    if i_gone > len(players_list) - 1:
+        i_gone = len(players_list) - i_gone
+    print('Выбывает человек под номером', players_list[i_gone])
+    players_list.remove(players_list[i_gone])
+    if i_gone > len(players_list) - 1:
+        start_count = players_list[0]
+    else:
+        start_count = players_list[i_gone]
+
+print('\nОстался человек под номером', players_list[i_gone])
