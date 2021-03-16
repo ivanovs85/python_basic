24733b32521d192011ea90e92909915936a5b07a
Semen <ivanovs85@mail.ru>
2021-03-16 18:09:34 +0300

    corrected

--- Module16/02_line/main.py
+++ Module16/02_line/main.py
@@ -1,29 +1,26 @@
 def range_class(start, end, step):
-    num = int((end - start) / step)
     class_list = []
-    for pupil in range(num + 1):
-        growth = start + step * pupil
-        class_list.append(growth)
+    for pupil in range(start, end, step):
+        class_list.append(pupil)
     return class_list
 
 
-# TODO: Правильнее было бы оформить эти значения в виде коонстант:
-#  то же самое только всее символы в нейминге верхнего регистра:)
-class_a_start = 160
-class_a_end = 176
-step_a = 2
-class_b_start = 162
-class_b_end = 180
-step_b = 3
+def sorting(sort_list):
+    for i_mn in range(len(sort_list)):
+        for curr in range(i_mn, len(sort_list)):
+            if sort_list[curr] < sort_list[i_mn]:
+                sort_list[curr], sort_list[i_mn] = sort_list[i_mn], sort_list[curr]
+    return sort_list
 
-# TODO: Предлагаю сделать на много проще: используйте range(), в котором вы также можете указать старт, стоп и шаг:)
-list_class = range_class(class_a_start, class_a_end, step_a)
-list_class.extend(range_class(class_b_start, class_b_end, step_b))
+CLASS_A_START = 160
+CLASS_A_END = 176
+STEP_A = 2
+CLASS_B_START = 162
+CLASS_B_END = 180
+STEP_B = 3
 
-# TODO: Предлагаю оформить в вид ф-ии, принимающей на вход не отсортированный список и возвращающей отсортированный:)
-for i_mn in range(len(list_class)):
-    for curr in range(i_mn, len(list_class)):
-        if list_class[curr] < list_class[i_mn]:
-            list_class[curr], list_class[i_mn] = list_class[i_mn], list_class[curr]
+list_class = range_class(CLASS_A_START, CLASS_A_END, STEP_A)
+list_class.extend(range_class(CLASS_B_START, CLASS_B_END, STEP_B))
+list_sort = sorting(list_class)
 
-print(list_class)
+print(list_sort)
