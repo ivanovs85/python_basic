b3a46894dec1079e1b72612b2ce6779b379430e7
Semen <ivanovs85@mail.ru>
2021-03-16 17:50:43 +0300

    Add lesson

--- Module16/01_scary_code/main.py
+++ Module16/01_scary_code/main.py
@@ -5,17 +5,15 @@ main_list.extend(list_a)
 count_a = 0
 count_b = 0
 
-# TODO: Нейминг вида "i_" больше подходит когда вы перебираете последовательность созданную при помощи range().
-#  Здесь же вполне конкртные элемеенты:)
-for i_elm in main_list:
-    if i_elm == 5:
+for elm in main_list:
+    if elm == 5:
         count_a += 1
-        main_list.remove(i_elm)
+        main_list.remove(elm)
 
 main_list.extend(list_b)
 
-for i_elm in main_list:
-    if i_elm == 3:
+for elm in main_list:
+    if elm == 3:
         count_b += 1
 
 print('Кол-во цифр 5 при первом объединении:', count_a,
